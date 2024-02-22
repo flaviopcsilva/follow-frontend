53a8429536e496377bde829e7cc71a8ddc8e03b8
Flávio Peres <flaviopc2@gmail.com>
2024-02-22 14:26:10 -0300

    Primeiro Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ConstrucaoComponent } from './paginas/construcao/construcao.component';
+import { LoginComponent } from './paginas/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ConstrucaoComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/paginas/construcao/construcao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-construcao',
+  standalone: true,
+  imports: [],
+  templateUrl: './construcao.component.html',
+  styleUrl: './construcao.component.css'
+})
+export class ConstrucaoComponent {
+
+}
